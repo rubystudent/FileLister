0af24e5febf1202d8e069b96821c169cd848a34d
rubystudent <cfgcl18@ukr.net>
2016-06-08 18:45:11 +0300

    v 1.3

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
   resources :file_lists, only:[:index]
-  get '/:filepath' => 'file_lists#index', filepath: %r([0-9a-zA-Zа-яА-ЯёЁ\/\%\-+_\s]+)
-
   root 'file_lists#index'
 end
